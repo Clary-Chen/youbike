d92c0fc24ed4ea86d0e82dd99ee2b013fb0554eb
BestIreliaAsia <tingclary1111@gmail.com>
2023-09-12 16:18:53 +0800

    feat: 修改mobile和pc的navbar中按鈕顏色會隨當前路由pathname改變

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate,useLocation } from 'react-router-dom'
 import './MobileNavbar.scss'
 
 export default function MobileNavbar() {
+    // 當前路由
+    const currentPathname = useLocation().pathname
+    // 跳轉路由變數
     const navigate = useNavigate()
     return (
         <div className='mobile-navbar'>
             <div className='mobile-titles'>
-                <div onClick={()=>navigate('/instructions')}>使用說明</div>
-                    <div onClick={()=>navigate('/charging')}>收費方式</div>
-                    <div onClick={()=>navigate('/')}>站點資訊</div>
-                    <div onClick={()=>navigate('/news')}>最新消息</div>
-                    <div onClick={()=>navigate('/activities')}>活動專區</div>
-                </div>
+                <div className={currentPathname==='/instructions'? 'chosen-color':''} onClick={()=>navigate('/instructions')}>使用說明</div>
+                <div className={currentPathname==='/charging'? 'chosen-color':''} onClick={()=>navigate('/charging')}>收費方式</div>
+                <div className={currentPathname==='/'? 'chosen-color':''} onClick={()=>navigate('/')}>站點資訊</div>
+                <div className={currentPathname==='/news'? 'chosen-color':''}onClick={()=>navigate('/news')}>最新消息</div>
+                <div className={currentPathname==='/activities'? 'chosen-color':''} onClick={()=>navigate('/activities')}>活動專區</div>
+            </div>
             <div className='mobile-login-button'>登入</div>
         </div>
     )
--- src/components/PcNavbar.jsx
+++ src/components/PcNavbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate,useLocation  } from 'react-router-dom'
 import './PcNavbar.scss'
 export default function PcNavbar() {
+  // 當前路由
+  const currentPathname = useLocation().pathname
   // 跳轉路由變數
   const navigate = useNavigate()
   return (
     <div className='pc-navbar'>
         <div className='pc-titles'>
-            <div onClick={()=>navigate('/instructions')}>使用說明</div>
-            <div onClick={()=>navigate('/charging')}>收費方式</div>
-            <div onClick={()=>navigate('/')}>站點資訊</div>
-            <div onClick={()=>navigate('/news')}>最新消息</div>
-            <div onClick={()=>navigate('/activities')}>活動專區</div>
+            <div className={currentPathname==='/instructions'? 'chosen-color':''} onClick={()=>navigate('/instructions')}>使用說明</div>
+            <div className={currentPathname==='/charging'? 'chosen-color':''} onClick={()=>navigate('/charging')}>收費方式</div>
+            <div className={currentPathname==='/'? 'chosen-color':''} onClick={()=>navigate('/')}>站點資訊</div>
+            <div className={currentPathname==='/news'? 'chosen-color':''}onClick={()=>navigate('/news')}>最新消息</div>
+            <div className={currentPathname==='/activities'? 'chosen-color':''} onClick={()=>navigate('/activities')}>活動專區</div>
         </div>
         <div className='pc-login-button'>登入</div>
     </div>
